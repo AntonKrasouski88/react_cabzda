52bd4da770cc958c7c3ec455ab22ab0f9822bca4
AntonKrasouski <antohkad21@gmail.com>
2022-09-17 11:43:50 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./Components/Accordion/Accordion";
+import {Rating} from "./Components/Rating/Rating";
+import {OnOff} from "./Components/OnOff/OnOff"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <PageTitle title = "This is App component"/>
+            <PageTitle title = "Hello User"/>
+            <Rating value = {3}/>
+            <Accordion titleValue = {"Menu first"} collapsed = {true}/>
+            <Accordion titleValue = {"Menu Second"} collapsed = {false}/>
+            <Rating value = {0}/>
+            <Rating value = {1}/>
+            <Rating value = {2}/>
+            <Rating value = {3}/>
+            <Rating value = {4}/>
+            <Rating value = {5}/>
+            <OnOff/>
+        </div>
+    );
 }
+
+type PageTitlePropsType = {
+    title: string;
+}
+
+let PageTitle = (props: PageTitlePropsType) => {
+    return <h1>{ props.title }</h1>
+}
+console.log("PageTitle rendering")
 
 export default App;
